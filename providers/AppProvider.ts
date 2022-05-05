e3c4babfa06235ae36f2312723139ead0a7218df
Julien Ripouteau <julien@ripouteau.com>
2022-05-05 12:30:01 +0200

    chore: test 2

--- providers/AppProvider.ts
+++ providers/AppProvider.ts
@@ -5,15 +5,15 @@ export default class AppProvider {
 
   public test() {
     console.log('hello')
+
+    console.log('hello 2')
   }
 
   public register() {
     this.test()
-    // Register your own bindings
   }
 
   public async boot() {
-    // IoC container is ready
   }
 
   public async ready() {
